e151fdc2c538e3a3fddf519d7d819a7f94843cab
Chase Hamby <c.chase.hamby@gmail.com>
2019-01-09 18:16:45 -0600

    setup

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
