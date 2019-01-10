63adb66821083879836d8bca31fae247671f310c
Chase Hamby <c.chase.hamby@gmail.com>
2019-01-09 18:59:01 -0600

    added public and private route

---
+++ src/components/pages/Home/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './home.scss';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="Home">
+        <h2>Home Component</h2>
+      </div>
+    );
+  }
+}
+
+export default Home;
